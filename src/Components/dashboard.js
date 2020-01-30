5b0c3655cd3ccb725bd89ca14a4bb175793c3a52
Boureima Tapily <boureimatapily1@gmail.com>
2020-01-30 19:57:45 +0000

    Add icons

--- src/Components/dashboard.js
+++ src/Components/dashboard.js
@@ -3,14 +3,26 @@ import React from 'react'
 const Dashboard = () => {
     return (
         <div>
-            <div class="card">
-                <div class="card-header">
-                    Featured
-  </div>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-4">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action active">
+                                FREELANCER
+                             </a>
+                            <a href="#" class="list-group-item list-group-item-action"> Overview</a>
+                            <a href="#" class="list-group-item list-group-item-action">Members</a>
+                            <a href="#" class="list-group-item list-group-item-action">Rapports</a>
+                            <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">Schedule</a>
+                            <a href="#" class="list-group-item list-group-item-action">Invoice</a>
+                            <a href="#" class="list-group-item list-group-item-action">Marketing</a>
+                            <a href="#" class="list-group-item list-group-item-action"><img src="/assets/img/bootstrap.svg" alt="" width="32" height="32" title="Bootstrap" />Setting</a>
+                        </div>
+                    </div>
+
+                    <div className="col-8">
+
+                    </div>
                 </div>
             </div>
         </div>
